e06633ec7d551620dc96707880860fb165ae16d2
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-06-03 18:29:39 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme,{shallow} from 'enzyme';
+import EnzymeAdapter  from '@wojtekmaj/enzyme-adapter-react-17';
+Enzyme.configure({adapter: new EnzymeAdapter()})
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']")
+  expect(appComponent.length).toBe(1)
+});
+test('renders increment button',()=>{
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+test('renders counter display',()=>{
+
+});
+test('counter display starts at 0',()=>{
+
+});
+test('clicking button increments counter display',()=>{
+
 });
