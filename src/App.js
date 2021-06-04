146c6261f4b4e0ac4cb2e0914495f0813752404d
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-06-04 12:09:58 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count,setCount] = useState(0)
   return (
-    <div className="App">
-      <h1 data-test='component-app'>unit test</h1>
+    <div className="App" data-test='component-app'>
+      <h1 data-test='component-display'>Display Counter <span data-test="component-count">{count}</span></h1>
+      <button data-test='component-button' onClick={()=>setCount(count+1)}>Increment</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,20 +3,35 @@ import App from './App';
 import Enzyme,{shallow} from 'enzyme';
 import EnzymeAdapter  from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({adapter: new EnzymeAdapter()})
+
+  const setup=() => shallow(<App />);
+  const findByTestArr =(wrapper,val)=>wrapper.find(`[data-test='${val}']`)
 test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findByTestArr(wrapper,'component-app');
   expect(appComponent.length).toBe(1)
 });
 test('renders increment button',()=>{
-
+  const wrapper = setup();
+  const button = findByTestArr(wrapper,'component-button');
+  expect(button.length).toBe(1)
 });
 test('renders counter display',()=>{
+  const wrapper = setup();
+  const display = findByTestArr(wrapper,'component-display');
+  expect(display.length).toBe(1)
 
 });
 test('counter display starts at 0',()=>{
-
+  const wrapper = setup();
+  const count = findByTestArr(wrapper,'component-count').text();
+  expect(count).toBe("0")
 });
 test('clicking button increments counter display',()=>{
+  const wrapper = setup();
+  const incrementButton = findByTestArr(wrapper,'component-button');
+  incrementButton.simulate('click')
+  const count = findByTestArr(wrapper,'component-count').text();
+  expect(count).toBe("1")
 
 });
