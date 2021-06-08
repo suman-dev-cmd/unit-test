3dd495adda022fee10824701aad76d1c0f7c42d4
suman-dev-cmd <sumanj@capitalnumbers.com>
2021-06-08 17:30:21 +0530

    create acction and store and test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Congrats from './Congrats';
 import GussedWord from './GussedWord';
+import Input from './Input';
+import {getSecretWord} from './actions'
 function App() {
   const [count,setCount] = useState(0)
+  const success = false;
+  const secrectWord = 'party';
+  const gussedWords = [];
+  useEffect(()=>{
+    getSecretWord();
+  },[])
   return (
     <div className="App" data-test='component-app'>
       <h1 data-test='component-display'>Display Counter <span data-test="component-count">{count}</span></h1>
       <button data-test='component-button' onClick={()=>setCount(count+1)}>Increment</button>
-      <Congrats success={true}/>
-      <GussedWord gussedWords={[
-         {gussedWord:'train',latterMatchCount:3},
-         {gussedWord:'agile',latterMatchCount:1},
-         {gussedWord:'party',latterMatchCount:5},
-      ]} />
+      <Congrats success={success}/>
+      <Input success={success} secrectWord={secrectWord} />
+      <GussedWord gussedWords={gussedWords} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,37 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Enzyme,{shallow} from 'enzyme';
-import EnzymeAdapter  from '@wojtekmaj/enzyme-adapter-react-17';
-Enzyme.configure({adapter: new EnzymeAdapter()})
+import {mount} from 'enzyme';
+import {findByTestArr} from '../test/testUtils';
+jest.mock('./actions');
+import {getSecretWord as mockGetSecretWord} from './actions';
 
-  const setup=() => shallow(<App />);
-  const findByTestArr =(wrapper,val)=>wrapper.find(`[data-test='${val}']`)
-test('renders without error', () => {
-  const wrapper = setup();
+const setup =()=>{
+  return mount(<App />);
+}
+test('reders without error', () => {
+  const wrapper= setup();
   const appComponent = findByTestArr(wrapper,'component-app');
-  expect(appComponent.length).toBe(1)
+  expect(appComponent).toHaveLength(1);
 });
-test('renders increment button',()=>{
-  const wrapper = setup();
-  const button = findByTestArr(wrapper,'component-button');
-  expect(button.length).toBe(1)
+describe('get serect word',()=>{
+  beforeEach(()=>{
+    mockGetSecretWord.mockClear();
+  })
+  test('getSecrectWord on app mount',()=>{
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+  test('getSecretWord does not run app update',()=>{
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
-test('renders counter display',()=>{
-  const wrapper = setup();
-  const display = findByTestArr(wrapper,'component-display');
-  expect(display.length).toBe(1)
 
-});
-test('counter display starts at 0',()=>{
-  const wrapper = setup();
-  const count = findByTestArr(wrapper,'component-count').text();
-  expect(count).toBe("0")
-});
-test('clicking button increments counter display',()=>{
-  const wrapper = setup();
-  const incrementButton = findByTestArr(wrapper,'component-button');
-  incrementButton.simulate('click')
-  const count = findByTestArr(wrapper,'component-count').text();
-  expect(count).toBe("1")
 
-});
